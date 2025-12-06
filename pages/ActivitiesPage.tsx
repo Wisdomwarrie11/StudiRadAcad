06ed516857d3065c8ab15344b9547a51e1382172
wisdom <wisdomwarrie11@gmail.com>
2025-12-06 16:25:59 +0100

    changes

--- pages/ActivitiesPage.tsx
+++ pages/ActivitiesPage.tsx
@@ -19,7 +19,7 @@ const ActivitiesPage = () => {
       description:
         "A 6-day intensive challenge tailored to your level. Physics, Technique, MRI, CT and more. Join the leaderboard!",
       image:
-        "https://images.unsplash.com/photo-1532938911079-1b06ac7ceec7?ixlib=rb-4.0.3&auto=format&fit=crop&w=1632&q=80",
+        "Radstudent.jpg",
       link: loggedIn ? "/challenge/dashboard" : "/challenge",
       badge: "New Feature",
       isHot: true,
