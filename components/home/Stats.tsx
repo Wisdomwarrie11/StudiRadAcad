28cf20353b3c53f913c50481fa6f187211273346
wisdom <wisdomwarrie11@gmail.com>
2026-09-12 09:41:50 +0100

    changes

--- components/home/Stats.tsx
+++ components/home/Stats.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const stats = [
-  { value: "800", label: "Community members", suffix: "+" },
+  { value: "1K", label: "Community members", suffix: "+" },
   { value: "10", label: "Expert Tutors", suffix: "+" },
   { value: "50", label: "Materials", suffix: "+" },
   { value: "24", label: "Career Placement", suffix: "%" },
